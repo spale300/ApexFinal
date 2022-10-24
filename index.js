7507b0b19e8f64930adf40431394d37bb76ee670
Stefan Spasic <93154683+spale300@users.noreply.github.com>
2022-10-24 19:37:30 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+// const yearEl = document.querySelectorAll(".year");
+// const currentYear = new Date().getFullYear();
+
+// console.log(yearEl);
+// yearEl.textContent = currentYear;
+
+const hero = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(hero);
